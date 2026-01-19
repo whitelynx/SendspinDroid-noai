b5cbe46eb978cdd9eb9e52dfc9896e2c7f588d2f
Chris <chris@sicmedia.net>
2026-01-18 22:01:09 -0600

    Update AGP from 8.13.2 to 9.0.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,9 +4,9 @@
 plugins {
     // Android Gradle Plugin (AGP) - manages Android build process
     // Version 8.13.2 is current stable (Dec 2025), supports API 36
-    id("com.android.application") version "8.13.2" apply false
+    id("com.android.application") version "9.0.0" apply false
 
     // Kotlin Android plugin - enables Kotlin compilation for Android
     // Version 2.1.0 is compatible with AGP 8.13.x
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
 }
